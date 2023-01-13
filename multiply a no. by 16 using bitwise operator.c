8b98a7cc94bf516c09f569cbe86cd372e9a11163
HarshG04 <122627106+HarshG04@users.noreply.github.com>
2023-01-13 21:53:44 +0530

    Create multiply a no. by 16 using bitwise operator.c

---
+++ multiply a no. by 16 using bitwise operator.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    
+    printf("Enter Any number : ");
+    scanf("%d",&a);
+    
+    b = a<<4;
+    
+    printf("%d x 16 = %d",a,b);
+    return 0;
+} 
