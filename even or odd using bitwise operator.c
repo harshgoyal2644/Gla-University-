3ce5d31bca757e7f4408a1f84a07cee115f865ff
HarshG04 <122627106+HarshG04@users.noreply.github.com>
2023-01-13 21:52:52 +0530

    Create even or odd using bitwise operator.c

---
+++ even or odd using bitwise operator.c
@@ -0,0 +1,18 @@
+
+
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter any number : ");
+    scanf("%d",&a);
+    
+   if (a&1)
+   printf("odd");
+   
+   else printf("even");
+    return 0;
+    
+}
+
+
