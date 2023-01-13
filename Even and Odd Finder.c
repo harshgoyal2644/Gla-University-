0a170a747737dfc9b2c34ccf631b773884524131
HarshG04 <122627106+HarshG04@users.noreply.github.com>
2023-01-13 21:34:48 +0530

    Create Even and Odd Finder.c

---
+++ Even and Odd Finder.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the value = ");
+    scanf("%d",&a);
+    if (a%2==0)
+    {
+    printf("Your value %d is even",a);
+    }
+    else {
+    printf("Your value %d is odd",a);
+    }
+    return 0;
+    
+    
+}
